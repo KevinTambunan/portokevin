e7e777bad0a0ae185f1c4cb04d3d9e3ff2b193cc
indah elisa sihombing <112603417+indahelisasihombing@users.noreply.github.com>
2023-09-19 11:11:02 +0700

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,171 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Portofolio Kevin</title>
+        <link rel="icon" type="image/x-icon" href="assets/img/favicon.ico" />
+        <!-- Font Awesome icons (free version)-->
+        <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
+        <!-- Google fonts-->
+        <link href="https://fonts.googleapis.com/css?family=Saira+Extra+Condensed:500,700" rel="stylesheet" type="text/css" />
+        <link href="https://fonts.googleapis.com/css?family=Muli:400,400i,800,800i" rel="stylesheet" type="text/css" />
+        <!-- Core theme CSS (includes Bootstrap)-->
+        <link href={{asset('asset/css/index.css')}} rel="stylesheet" />
+    </head>
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="sideNav">
+            <a class="navbar-brand js-scroll-trigger" href="#page-top">
+                <span class="d-block d-lg-none">Kevin Yoyada Tambunan</span>
+                <span class="d-none d-lg-block"><img class="img-fluid img-profile rounded-circle mx-auto mb-2" src={{asset('asset/image/kevin.png')}} alt="..." /></span>
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+            <div class="collapse navbar-collapse" id="navbarResponsive">
+                <ul class="navbar-nav">
+                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#about">About</a></li>
+                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#experience">Experience</a></li>
+                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#education">Education</a></li>
+                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#skills">Skills</a></li>
+                </ul>
+            </div>
+        </nav>
+        <!-- Page Content-->
+        <div class="container-fluid p-0">
+            <!-- About-->
+            <section class="resume-section" id="about">
+                <div class="resume-section-content">
+                    <h1 class="mb-0">
+                        Kevin Y.
+                        <span class="text-primary">Tambunan</span>
+                    </h1>
+                    <div class="subheading mb-5">
+                        Sitoluama, Laguboti, TOBA, Sumatera Utara
+                        <a href="mailto:name@email.com">tambunankevin8@gmail.com</a>
+                    </div>
+                    <p class="lead mb-5">
+                        I am Kevin Yoyada Tambunan, a person who has experience using PHP since 4 years ago when I started studying programming at college. I have created several websites such as e-commerce for UMKM and websites for tourists around Lake Toba and created a Learning Management System (LMS) website for an institute using HTML, CSS, Javascript and PHP with the Laravel framework. I also like to learn new things, especially with the rapid development of technology.
+                    </p>
+                    <div class="social-icons">
+                        <a class="social-icon" href="https://www.linkedin.com/in/kevin-tambunan-4b0b311a9/"><i class="fab fa-linkedin-in"></i></a>
+                        <a class="social-icon" href="https://github.com/KevinTambunan"><i class="fab fa-github"></i></a>
+                        <a class="social-icon" href="https://www.instagram.com/yoyada16/"><i class="fab fa-instagram"></i></a>
+                        <a class="social-icon" href="https://web.facebook.com/kevin.yoyada"><i class="fab fa-facebook-f"></i></a>
+                    </div>
+                </div>
+            </section>
+            <hr class="m-0" />
+            <!-- Experience-->
+            <section class="resume-section" id="experience">
+                <div class="resume-section-content">
+                    <h2 class="mb-5">Experience</h2>
+                    <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+                        <div class="flex-grow-1">
+                            <h3 class="mb-0">E-commerce for UMKM</h3>
+                            <div class="subheading mb-3">Fullstack Enginner</div>
+                            <p>
+                                Developing 5 e-commerce sites for local UMKM that sell various handicraft products and processed foods. E-commerce is developed in the form of a website that can be reached by potential buyers and can carry out transactions on it. This website was developed using PHP with the Laravel framework.
+                            </p>
+                            <span>Link Websites</span>
+                            <ul >
+                                <li><a href="https://meinoelkeripikpisang.satutujutrack.com/">Meinoel Keripik</a></li>
+                                <li><a href="https://tabonaytelatela.satutujutrack.com/indek.php">Tabonay Tela-tela</a></li>
+                                <li><a href="https://tabotoba.satutujutrack.com/">Tabo Toba</a></li>
+                                <li><a href="https://jofebakery.satutujutrack.com/">Jofe Bakery</a></li>
+                                <li><a href="https://fmtoba.satutujutrack.com/">FMToba</a></li>
+                            </ul>
+                        </div>
+                        <div class="flex-shrink-0"><span class="text-primary">Jan 2023 - Sep 2023</span></div>
+                    </div>
+                    <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+                        <div class="flex-grow-1">
+                            <h3 class="mb-0">Redevelop LMS</h3>
+                            <div class="subheading mb-3">Fullstack Enginner</div>
+                            <p>Create or redevelop a learning management system website at a local higher education institute in the North Sumatra area. This LMS was remade using the PHP programming language and the Laravel framework.</p>
+                        </div>
+                        <div class="flex-shrink-0"><span class="text-primary">Mei 2022 - Sept 2022</span></div>
+                    </div>
+                    <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+                        <div class="flex-grow-1">
+                            <h3 class="mb-0">Build 13 tourist village websites</h3>
+                            <div class="subheading mb-3">Fullstack Enginner</div>
+                            <p>Build 13 tourist village websites around Lake Toba under the tutelage of lecturers in collaboration with the Ministry of Tourism and Creative Economy of the Republic of Indonesia</p>
+                            <span>Some Websites</span>
+                            <ul >
+                                <li style="text-decoration: none;"><a href="http://desawisatatarabunga.com/">Desa Wisata Tarabunga</a></li>
+                                <li><a href="http://desawisatameat.com/">Desa Wisata Meat</a></li>
+                                <li><a href="http://desawisatahutaginjang.com/">Desa Wisata Tarabunga</a></li>
+                            </ul>
+                        </div>
+                        <div class="flex-shrink-0"><span class="text-primary">December 2021 - Mei 2022</span></div>
+                    </div>
+                </div>
+            </section>
+            <hr class="m-0" />
+            <!-- Education-->
+            <section class="resume-section" id="education">
+                <div class="resume-section-content">
+                    <h2 class="mb-5">Education</h2>
+                    <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+                        <div class="flex-grow-1">
+                            <h3 class="mb-0">Del Institute of Technology</h3>
+                            <div class="subheading mb-3">Bachelor of Technology</div>
+                            <div>Bachelor of Technology - Vocational Faculty</div>
+                            <p>GPA: 3.26</p>
+                        </div>
+                        <div class="flex-shrink-0"><span class="text-primary">Sept 2019 - Sept 2023</span></div>
+                    </div>
+                    <div class="d-flex flex-column flex-md-row justify-content-between">
+                        <div class="flex-grow-1">
+                            <h3 class="mb-0">SMA N 2 Balige High School</h3>
+                            <div class="subheading mb-3">Mathematics and Natural Sciences Program</div>
+                        </div>
+                        <div class="flex-shrink-0"><span class="text-primary">June 2016 - May 2019</span></div>
+                    </div>
+                </div>
+            </section>
+            <hr class="m-0" />
+            <!-- Skills-->
+            <section class="resume-section" id="skills">
+                <div class="resume-section-content">
+                    <h2 class="mb-5">Skills</h2>
+                    <div class="subheading mb-3">Programming Languages & Tools</div>
+                    <ul class="list-inline dev-icons">
+                        <li class="list-inline-item"><i class="fab fa-php"></i></li>
+                        <li class="list-inline-item"><i class="fa-brands fa-laravel"></i></li>
+                        <li class="list-inline-item"><i class="fab fa-html5"></i></li>
+                        <li class="list-inline-item"><i class="fab fa-css3-alt"></i></li>
+                        <li class="list-inline-item"><i class="fab fa-js-square"></i></li>
+                        <li class="list-inline-item"><i class="fab fa-node-js"></i></li>
+                        <li class="list-inline-item"><i class="fa-solid fa-database"></i></li>
+                        <li class="list-inline-item"><i class="fab fa-java"></i></li>
+                        <li class="list-inline-item"><i class="fab fa-python"></i></li>
+                        <li class="list-inline-item"><i class="fab fa-golang"></i></li>
+                        <li class="list-inline-item"><i class="fab fa-npm"></i></li>
+                    </ul>
+                    <div class="subheading mb-3">Another Skills</div>
+                    <ul class="fa-ul mb-0">
+                        <li>
+                            <span class="fa-li"><i class="fas fa-check"></i></span>
+                            Good Communication
+                        </li>
+                        <li>
+                            <span class="fa-li"><i class="fas fa-check"></i></span>
+                            Testing & Debugging
+                        </li>
+                        <li>
+                            <span class="fa-li"><i class="fas fa-check"></i></span>
+                            Agile Development & Scrum
+                        </li>
+                    </ul>
+                </div>
+            </section>
+        </div>
+        <!-- Bootstrap core JS-->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src={{asset('asset/js/index.js')}}></script>
+    </body>
+</html>
